0b5d3e5e036777af4e5e637af604d0de5907094c
Phillipe Roger Souza <phillipe.souza@rte.com.br>
2020-08-20 12:12:00 -0300

    Tratamento de notificações

--- src/NerdStore.WebApp.MVC/Controllers/CarrinhoController.cs
+++ src/NerdStore.WebApp.MVC/Controllers/CarrinhoController.cs
@@ -4,6 +4,7 @@ using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using NerdStore.Catalogo.Application.Services;
 using NerdStore.Core.Communication.Mediator;
+using NerdStore.Core.Messages.CommonMessages.Notifications;
 using NerdStore.Vendas.Application.Commands;
 
 namespace NerdStore.WebApp.MVC.Controllers
@@ -13,9 +14,10 @@ namespace NerdStore.WebApp.MVC.Controllers
         private readonly IProdutoAppService _produtoAppService;
 		private readonly IMediatorHandler _mediatorHandler;
 
-		public CarrinhoController(IProdutoAppService produtoAppService, 
+		public CarrinhoController(INotificationHandler<DomainNotification> notifications,
+								  IProdutoAppService produtoAppService, 
                                   IMediatorHandler mediatorHandler
-                                 ) 
+                                 ) : base(notifications, mediatorHandler)
         {
             _produtoAppService = produtoAppService;
             _mediatorHandler = mediatorHandler;            
@@ -43,7 +45,12 @@ namespace NerdStore.WebApp.MVC.Controllers
             var command = new AdicionarItemPedidoCommand(ClienteId, produto.Id, produto.Nome, quantidade, produto.Valor);
 			await _mediatorHandler.EnviarComando(command);
 
-			TempData["Erro"] = "Produto indisponível";
+			if (OperacaoValida())
+			{
+				return RedirectToAction("Index");
+			}
+
+			TempData["Erro"] = ObterMensagensErro();
 			return RedirectToAction("ProdutoDetalhe", "Vitrine", new { id });
         }
 
